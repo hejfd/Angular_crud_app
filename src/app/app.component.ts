481fb7789aa4b83c31c3a6c225d089c7623331ba
Jerin James <jerinjames672@gmail.com>
2024-09-12 17:17:12 +0530

    emploee add edit component is created and integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,29 @@ import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog'
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
 
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,MatToolbarModule,MatIconModule,
     MatButtonModule,
+    MatDialogModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sample_project';
+
+  constructor(private _dialog:MatDialog){
+
+  }
+  
+  openAddEditEmpForm(){
+    this._dialog.open(EmpAddEditComponent)
+  }
 }
- 
+
